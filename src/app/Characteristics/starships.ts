c8ca367a3b460309fbaca5a66b1000f43fbf291d
Eugeneconstant <eugene.constant@isen.yncrea.fr>
2020-12-17 15:38:38 +0100

    constructeur vehicules et starships

---
+++ src/app/Characteristics/starships.ts
@@ -0,0 +1,45 @@
+
+
+// tslint:disable-next-line: class-name
+export class starships{
+  name: string;
+  model: string;
+  manufacturer: string;
+  // tslint:disable-next-line: variable-name
+  costs_in_credits: number;
+  length: number;
+  // tslint:disable-next-line: variable-name
+  max_atmosphering_speed: number;
+  crew: string;
+  passengers: number;
+  // tslint:disable-next-line: variable-name
+  cargo_capacity: number;
+  consumables: string;
+  // tslint:disable-next-line: variable-name
+  hyperdrive_rating: Float32Array;
+  MGLT: number;
+  // tslint:disable-next-line: variable-name
+  starships_class: string;
+  pilots: Array<number>;
+  created: Date;
+  edited: Date;
+
+  // tslint:disable-next-line: no-shadowed-variable
+  constructor(starships?: any)
+  {
+    this.name = starships.name;
+    this.created = starships.created;
+    this.model = starships.model;
+    this.cargo_capacity = starships.cargo_capacity;
+    this.MGLT = starships.MGLT;
+    this.pilots = starships.pilots;
+    this.edited = starships.edited;
+    this.hyperdrive_rating = starships.hyperdrive_rating;
+    this.consumables = starships.consumables;
+    this.starships_class = starships.starships_class;
+    this.max_atmosphering_speed = starships.max_atmosphering_speed;
+    this.length = starships.length;
+    this.passengers = starships.passengers;
+    this.manufacturer = starships.manufacturer;
+  }
+}
---
+++ src/app/Characteristics/vehicles.ts
@@ -0,0 +1,42 @@
+// tslint:disable-next-line: class-name
+export class vehicles{
+  name: string;
+  model: string;
+  manufacturer: string;
+  // tslint:disable-next-line: variable-name
+  cost_in_credits: number;
+  length: Float32Array;
+  // tslint:disable-next-line: variable-name
+  max_astmosphering_speed: number;
+  crew: number;
+  passengers: number;
+  // tslint:disable-next-line: variable-name
+  cargo_capacity: number;
+  consumables: string;
+  // tslint:disable-next-line: variable-name
+  vehicle_class: string;
+  pilots: Array<number>;
+  created: Date;
+  edited: Date;
+
+  // tslint:disable-next-line: no-shadowed-variable
+  constructor(vehicles?: any)
+  {
+    this.name = vehicles.name;
+    this.model = vehicles.model;
+    this.manufacturer = vehicles.manufacturer;
+    this.cost_in_credits = vehicles.cost_in_credits;
+    this.created = vehicles.created;
+    this.pilots = vehicles.pilots;
+    this.created = vehicles.created;
+    this.cargo_capacity = vehicles.cargo_capacity;
+    this.vehicle_class = vehicles.vehicle_class;
+    this.crew = vehicles.crew;
+    this.consumables = vehicles.consumables;
+    this.edited = vehicles.edited;
+    this.length = vehicles.length;
+    this.passengers = vehicles.passengers;
+  }
+}
+
+
